942f1bf26bf0c478bd22318b25be067cb998b629
乔赫 <qiaohe@vackbot.com>
2021-05-12 18:04:40 +0800

    修改部分数据

--- src/store/getters.js
+++ src/store/getters.js
@@ -2,6 +2,7 @@ const getters = {
   sidebar: state => state.app.sidebar,
   device: state => state.app.device,
   token: state => state.user.token,
+  csrftoken: state => state.user.csrftoken,
   avatar: state => state.user.avatar,
   name: state => state.user.name,
   username: state => state.user.username,
